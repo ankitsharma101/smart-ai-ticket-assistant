760d9906364d36c435c91cf24fca17113afc84b7
ankitsharma101 <ankitsharma101.exe@gmail.com>
2025-07-25 16:22:12 +0530

    Added required id for Inngest

--- ai-ticket-assistant/inngest/client.js
+++ ai-ticket-assistant/inngest/client.js
@@ -1,6 +1,5 @@
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
-  name: "AI Ticket Assistant",
-  eventKey: process.env.INNGEST_EVENT_KEY,
+  id: "ai-ticket-assistant", // ✅ required
 });
